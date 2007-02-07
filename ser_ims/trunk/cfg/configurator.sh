7a9b1aafa12a454dcf38b678ad7bc6d4a68e2fe4
jsbach <jsbach@a36f3ba7-d720-0410-92d7-d12b36172079>
2007-02-07 19:34:20 +0000

    added support for passing arguments at command line

--- ser_ims/trunk/cfg/configurator.sh
+++ ser_ims/trunk/cfg/configurator.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Initialization & global vars
-clear
+# if you execute this script for the second time
+# you should change these variables to the latest
+# domain name and ip address
 DDOMAIN="open-ims\.test"
 DSDOMAIN="open-ims\\\.test"
 DEFAULTIP="127\.0\.0\.1"
@@ -16,41 +18,51 @@ read ip_address
 # input domain is to be slashed for cfg regexes 
 slasheddomain=`echo $domainname | sed 's/\./\\\\\\\\\./g'`
 
-printf "File to change [\"all\" for everything, \"exit\" to quit]:"
+	if [ $# != 0 ] 
+	then 
+	printf "changing: "
+	    for j in $* 
+	    do
+		sed -i -e "s/$DDOMAIN/$domainname/g" $j
+		sed -i -e "s/$DSDOMAIN/$slasheddomain/g" $j
+		sed -i -e "s/$DEFAULTIP/$ip_address/g" $j
+		printf "$j " 
+	    done
+	echo 
+	else 
+	printf "File to change [\"all\" for everything, \"exit\" to quit]:"
+	# loop
+	    while read filename ;
+	    do
+		    if [ "$filename" = "exit" ] 
+		    then 
+		    printf "exitting...\n"
+		    break ;
 
-# main loop
-    while read filename ;
-    do
-	    if [ "$filename" = "exit" ] 
-	    then 
-	    printf "exitting...\n"
-	    break ;
+			elif [ "$filename" = "all" ]
+			then    
+			    printf "changing: "
+			   for i in $CONFFILES 
+			   do
+				sed -i -e "s/$DDOMAIN/$domainname/g" $i
+				sed -i -e "s/$DSDOMAIN/$slasheddomain/g" $i
+				sed -i -e "s/$DEFAULTIP/$ip_address/g" $i
+				
+				printf "$i " 
+			   done 
+			   echo 
+			   break;
 
-		elif [ "$filename" = "all" ]
-		then    
-		    printf "changing: "
-		   for i in $CONFFILES 
-		   do
-			sed -i -e "s/$DDOMAIN/$domainname/g" $i
-			sed -i -e "s/$DSDOMAIN/$slasheddomain/g" $i
-			sed -i -e "s/$DEFAULTIP/$ip_address/g" $i
-			
-			printf "$i " 
-		   done 
-		   echo 
-		   break;
+				elif [ -w $filename ] 
+				then
+				    printf "changing $filename \n"
+				    sed -i -e "s/$DDOMAIN/$domainname/g" $filename
+				    sed -i -e "s/$DSDOMAIN/$slasheddomain/g" $filename
+				    sed -i -e "s/$DEFAULTIP/$ip_address/g" $filename
 
-			elif [ -w $filename ] 
-			then
-			    printf "changing $filename \n"
-			    sed -i -e "s/$DDOMAIN/$domainname/g" $filename
-			    sed -i -e "s/$DSDOMAIN/$slasheddomain/g" $filename
-			    sed -i -e "s/$DEFAULTIP/$ip_address/g" $filename
-
-				else 
-				printf "cannot access file $filename. skipping... \n" 
-	    fi
-
-	    printf "File to Change:"
-    done 
-
+					else 
+					printf "cannot access file $filename. skipping... \n" 
+		    fi
+		    printf "File to Change:"
+	    done 
+	fi
