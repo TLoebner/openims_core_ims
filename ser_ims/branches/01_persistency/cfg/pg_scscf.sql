ddf91cabf95bcf9523885091ba29bef78fc06a7c
mariojvp <mariojvp@a36f3ba7-d720-0410-92d7-d12b36172079>
2007-02-12 17:53:25 +0000

    Tables for auth information (postgres).

---
+++ ser_ims/branches/01_persistency/cfg/pg_scscf.sql
@@ -0,0 +1,15 @@
+CREATE TABLE auth_data_bulk (
+    snapshot_version INTEGER NOT NULL,
+    step_version INTEGER NOT NULL,
+    data BYTEA NOT NULL,
+    PRIMARY KEY (snapshot_version, step_version)
+);
+
+CREATE TABLE auth_data_cache (
+    snapshot_version INTEGER NOT NULL,
+    step_version INTEGER NOT NULL,
+    private VARCHAR(80) NOT NULL,
+    public  VARCHAR(128) NOT NULL,
+    data BYTEA NOT NULL,
+    PRIMARY KEY (snapshot_version, step_version, private, public)
+);
