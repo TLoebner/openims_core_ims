849f6cbc18b72a8a0624977db2021e8098995778
mariojvp <mariojvp@a36f3ba7-d720-0410-92d7-d12b36172079>
2007-02-19 19:06:45 +0000

    Load of auth/registrar/dialogs information from db at startup.

--- ser_ims/branches/01_persistency/modules/scscf/s_persistency.c
+++ ser_ims/branches/01_persistency/modules/scscf/s_persistency.c
@@ -49,6 +49,9 @@
  * S-CSCF persistency operations
  *
  *  \author Dragos Vingarzan vingarzan -at- fokus dot fraunhofer dot de
+ * 
+ * Database persistency implemented by Mário Ferreira (PT INOVAÇÃO)
+ * est-m-aferreira at ptinovacao.pt
  *
  */
 
@@ -69,8 +72,10 @@ extern int* dialogs_snapshot_version;
 extern int* dialogs_step_version;
 extern int* registrar_snapshot_version;
 extern int* registrar_step_version;
+extern gen_lock_t* db_lock; 
 
 int s_dump(bin_data* x, persistency_mode_t mode, char* location, char* prepend_fname);
+int s_load(bin_data *x,int mode,char *location,char* prepend_fname);
 
 /**
  * Creates a snapshots of the authorization data and then calls the dumping function.
@@ -106,58 +111,105 @@ int s_dump(bin_data* x, persistency_mode_t mode, char* location, char* prepend_f
 	
 	switch (mode){
 		case NO_PERSISTENCY:
-			LOG(L_ERR,"ERR:"M_NAME":bin_dump: Snapshot done but persistency was disabled...\n");
+			LOG(L_ERR,"ERR:"M_NAME":s_dump: Snapshot done but persistency was disabled...\n");
 			return 0;
 		case WITH_FILES:
 			return bin_dump_to_file(x,location,prepend_fname);
 		case WITH_DATABASE_BULK:
 			return bin_dump_to_db(x, prepend_fname);
 		case WITH_DATABASE_CACHE:
-			LOG(L_ERR,"ERR:"M_NAME":bin_dump: Snapshot done but WITH_DATABASE_CACHE not implemented...\n");
+			LOG(L_ERR,"ERR:"M_NAME":s_dump: Snapshot done but WITH_DATABASE_CACHE not implemented...\n");
 			return 0;
 		default:
-			LOG(L_ERR,"ERR:"M_NAME":bin_dump: Snapshot done but no such mode %d\n",mode);
+			LOG(L_ERR,"ERR:"M_NAME":s_dump: Snapshot done but no such mode %d\n",mode);
 			return 0;
 	}
 }  
 
-int bin_dump_auth_to_db(bin_data *x);
-int bin_dump_dialogs_to_db(bin_data *x);
-int bin_dump_registrar_to_db(bin_data *x);
+int bin_dump_to_table(bin_data *x, char* table, int snapshot_version, int step_version);
 
 /**
  * Writes the binary data to a snapshot on the DB.
  * @param x - the binary data to write
- * @param what - dump auth, dialog or registrar information
+ * @param prepend_fname - dump auth, dialog or registrar information
  * @returns 1 on success or 0 on failure
  */
 int bin_dump_to_db(bin_data *x, char* prepend_fname){
 	
+	char* table;
+	int snapshot_version;
+	int step_version;
+	
 	if(!strncmp(prepend_fname,"sregistrar",10)){
-		return bin_dump_registrar_to_db(x);
+		table="registrar_bulk";
+		snapshot_version=*registrar_snapshot_version;//registrar snapshot timer must increment it
+		step_version=*registrar_step_version;//registrar step timer must increment it
 	}
 	else{
 		if(!strncmp(prepend_fname,"sdialogs",8)){
-			return bin_dump_dialogs_to_db(x);
+			table="dialogs_bulk";
+			snapshot_version=*dialogs_snapshot_version;//dialogs snapshot timer must increment it
+			step_version=*dialogs_step_version;//dialogs step timer must increment it
 		}
 		else{
 			if(!strncmp(prepend_fname,"authdata",8)){
-				return bin_dump_auth_to_db(x);
+				table="auth_data_bulk";
+				snapshot_version=*auth_snapshot_version;//auth snapshot timer must increment it
+				step_version=*auth_step_version;//auth step timer must increment it
 			}
 			else{
 				LOG(L_ERR,"ERR:"M_NAME":bin_dump_to_db: No such information to dump %s\n", prepend_fname);
 				return 0;
 			}
 		}
-	}	
+	}
+	
+	return bin_dump_to_table(x, table, snapshot_version, step_version);
+}
+
+int bin_db_keep_count=1; /**< how many old snapshots to keep */
+
+/**
+ * Drops older auth/dialogs/registrar snapshots,
+ * keeping the 'bin_db_keep_count' most recent. 
+ * @param table - where to drop.
+ * @param current_snapshot_version - version of the current snapshot
+ * @returns 1 on success or 0 on failure
+ */
+int delete_older_snapshots(char* table, int current_snapshot){
+
+	db_key_t query_cols[1];
+	db_op_t  query_ops[1];
+	db_val_t query_vals[1];
+
+	query_cols[0] = "snapshot_version";
+	query_ops[0] = OP_LEQ;
+	query_vals[0].type = DB_INT;
+	query_vals[0].nul = 0;
+	query_vals[0].val.int_val = current_snapshot - bin_db_keep_count;
+
+	if (scscf_dbf.use_table(scscf_db, table) < 0) {
+		LOG(L_ERR, "ERR:"M_NAME":delete_older_snapshots(): Error in use_table\n");
+		return 0;
+	}
+
+	if (scscf_dbf.delete(scscf_db, query_cols, query_ops, query_vals, 1) < 0) {
+		LOG(L_ERR, "ERR:"M_NAME":delete_older_snapshots(): Error while deleting older snapshots\n");
+		return 0;
+	}
+	
+	return 1;
 }
 
 /**
- * Writes auth data to a snapshot on the DB.
+ * Writes auth/dialogs/registrar data to a snapshot on the DB.
  * @param x - the binary data to write
+ * @param table - where to dump.
+ * @param snapshot_version - version of the current snapshot
+ * @param step_version - the step in the current snapshot
  * @returns 1 on success or 0 on failure
  */
-int bin_dump_auth_to_db(bin_data *x){
+int bin_dump_to_table(bin_data *x, char* table, int snapshot_version, int step_version){
 	db_key_t keys[3];
 	db_val_t vals[3];
 
@@ -167,111 +219,45 @@ int bin_dump_auth_to_db(bin_data *x){
 
 	vals[0].type = DB_INT;
 	vals[0].nul = 0;
-	vals[0].val.int_val=*auth_snapshot_version;//auth snapshot timer must increment it
+	vals[0].val.int_val=snapshot_version;
 	
 	vals[1].type = DB_INT;
 	vals[1].nul = 0;
-	vals[1].val.int_val=*auth_step_version;//auth step timer must increment it
+	vals[1].val.int_val=step_version;
 
 	str d = {x->s, x->len};
 	vals[2].type = DB_BLOB;
 	vals[2].nul = 0;
 	vals[2].val.blob_val = d;
 
+	//lock
+	lock_get(db_lock);
 
-	if (scscf_dbf.use_table(scscf_db, "auth_data_bulk") < 0) {
-		LOG(L_ERR, "ERR:"M_NAME":bin_dump_auth_to_db(): Error in use_table\n");
+	if (scscf_dbf.use_table(scscf_db, table) < 0) {
+		LOG(L_ERR, "ERR:"M_NAME":bin_dump_to_table(): Error in use_table\n");
+		lock_release(db_lock);//unlock
 		return 0;
 	}
 
 	if (scscf_dbf.insert(scscf_db, keys, vals, 3) < 0) {
-		LOG(L_ERR, "ERR:"M_NAME":bin_dump_auth_to_db(): Error while inserting auth_userdata\n");
+		LOG(L_ERR, "ERR:"M_NAME":bin_dump_to_table(): Error while inserting on %s\n", table);
+		lock_release(db_lock);//unlock
 		return 0;
 	}
+	
+	//delete older snapshots
+	if (delete_older_snapshots(table, snapshot_version)!=1){
+		LOG(L_ERR, "ERR:"M_NAME":bin_dump_to_table(): Error while deleting older snapshots from %s\n", table);
+		lock_release(db_lock);//unlock
+		return 0;
+	}
+	
+	//unlock
+	lock_release(db_lock);
 	
 	return 1;
 }
 
-/**
- * Writes s_dialogs data to a snapshot on the DB.
- * @param x - the binary data to write
- * @returns 1 on success or 0 on failure
- */
-int bin_dump_dialogs_to_db(bin_data *x){
-	db_key_t keys[3];
-	db_val_t vals[3];
-
-	keys[0] = "snapshot_version";
-	keys[1] = "step_version";
-	keys[2] = "data";
-
-	vals[0].type = DB_INT;
-	vals[0].nul = 0;
-	vals[0].val.int_val=*dialogs_snapshot_version;//dialogs snapshot timer must increment it
-	
-	vals[1].type = DB_INT;
-	vals[1].nul = 0;
-	vals[1].val.int_val=*dialogs_step_version;//dialogs step timer must increment it
-
-	str d = {x->s, x->len};
-	vals[2].type = DB_BLOB;
-	vals[2].nul = 0;
-	vals[2].val.blob_val = d;
-
-
-	if (scscf_dbf.use_table(scscf_db, "dialogs_bulk") < 0) {
-		LOG(L_ERR, "ERR:"M_NAME":bin_dump_dialogs_to_db(): Error in use_table\n");
-		return 0;
-	}
-
-	if (scscf_dbf.insert(scscf_db, keys, vals, 3) < 0) {
-		LOG(L_ERR, "ERR:"M_NAME":bin_dump_dialogs_to_db(): Error while inserting s_dialogs\n");
-		return 0;
-	}
-	
-	return 1;
-}
-
-/**
- * Writes registrar data to a snapshot on the DB.
- * @param x - the binary data to write
- * @returns 1 on success or 0 on failure
- */
-int bin_dump_registrar_to_db(bin_data *x){
-	
-	db_key_t keys[3];
-	db_val_t vals[3];
-
-	keys[0] = "snapshot_version";
-	keys[1] = "step_version";
-	keys[2] = "data";
-
-	vals[0].type = DB_INT;
-	vals[0].nul = 0;
-	vals[0].val.int_val=*registrar_snapshot_version;//registrar snapshot timer must increment it
-	
-	vals[1].type = DB_INT;
-	vals[1].nul = 0;
-	vals[1].val.int_val=*registrar_step_version;//registrar step timer must increment it
-
-	str d = {x->s, x->len};
-	vals[2].type = DB_BLOB;
-	vals[2].nul = 0;
-	vals[2].val.blob_val = d;
-
-
-	if (scscf_dbf.use_table(scscf_db, "registrar_bulk") < 0) {
-		LOG(L_ERR, "ERR:"M_NAME":bin_dump_registrar_to_db(): Error in use_table\n");
-		return 0;
-	}
-
-	if (scscf_dbf.insert(scscf_db, keys, vals, 3) < 0) {
-		LOG(L_ERR, "ERR:"M_NAME":bin_dump_registrar_to_db(): Error while inserting registrar information\n");
-		return 0;
-	}
-	
-	return 1;
-}
 
 /**
  * Loads the authorization data from the last snapshot.
@@ -281,7 +267,8 @@ int load_snapshot_authdata()
 {
 	bin_data x;
 	auth_userdata *aud;
-	if (!bin_load(&x,scscf_persistency_mode,scscf_persistency_location,"authdata")) goto error;
+	if (!s_load(&x,scscf_persistency_mode,scscf_persistency_location,"authdata")) goto error;
+	//if (!bin_load(&x,scscf_persistency_mode,scscf_persistency_location,"authdata")) goto error;
 	bin_print(&x);
 	x.max=0;
 	LOG(L_INFO,"INFO:"M_NAME":load_snapshot_auth: max %d len %d\n",x.max,x.len);
@@ -304,6 +291,224 @@ error:
 	
 }
 
+int bin_load_from_db(bin_data *x, char* prepend_fname);
+
+int s_load(bin_data *x,int mode,char *location,char* prepend_fname){
+	
+	switch (mode){
+		case NO_PERSISTENCY:
+			LOG(L_ERR,"ERR:"M_NAME":s_load: Persistency support was disabled\n");
+			return 0;
+		case WITH_FILES:
+			return bin_load_from_file(x,location,prepend_fname);		
+		case WITH_DATABASE_BULK:
+			return bin_load_from_db(x, prepend_fname);
+		case WITH_DATABASE_CACHE:
+			LOG(L_ERR,"ERR:"M_NAME":s_load: WITH_DATABASE_CACHE not implemented...\n");
+			return 0;
+		default:
+			LOG(L_ERR,"ERR:"M_NAME":s_load: Can't resume because no such mode %d\n",mode);
+			return 0;
+	}
+}
+
+int bin_load_from_table(bin_data *x, char* table);
+
+/**
+ * Reloads the last snapshot dumped to db.
+ * @param x - where to load
+ * @param prepend_fname - load auth, dialog or registrar information
+ * @returns 1 on success, 0 on error
+ */
+int bin_load_from_db(bin_data *x, char* prepend_fname){
+	
+	char* table;
+	
+	if(!strncmp(prepend_fname,"sregistrar",10)){
+		table="registrar_bulk";
+	}
+	else{
+		if(!strncmp(prepend_fname,"sdialogs",8)){
+			table="dialogs_bulk";
+		}
+		else{
+			if(!strncmp(prepend_fname,"authdata",8)){
+				table="auth_data_bulk";
+			}
+			else{
+				LOG(L_ERR,"ERR:"M_NAME":bin_load_from_db: No such information to load %s\n", prepend_fname);
+				return 0;
+			}
+		}
+	}
+	
+	return bin_load_from_table(x, table);
+}
+
+int db_get_last_snapshot_version(char* table, int* version);
+int set_versions(char* table, int snapshot_version, int step_version);
+
+/**
+ * Reloads the last snapshot dumped to db.
+ * @param x - where to load
+ * @param table - load auth, dialog or registrar information
+ * @returns 1 on success, 0 on error
+ */
+int bin_load_from_table(bin_data *x, char* table){
+	
+	int snapshot_version;
+	int r;
+	bin_alloc(x,1024);//due to bin_free() on load_snapshot_...()
+	
+	//lock
+	lock_get(db_lock);
+	
+	if((r=db_get_last_snapshot_version(table, &snapshot_version))==0){
+		LOG(L_ERR,"ERR:"M_NAME":bin_load_from_table: Error while getting snapshot version\n");
+		lock_release(db_lock);//unlock
+		return 0;
+	}
+	else if(r==-1){//empty table, nothing to load
+			LOG(L_INFO,"INFO:"M_NAME":bin_load_from_table: %s empty\n", table);
+			lock_release(db_lock);//unlock
+			return 1;
+		}
+		
+	//set snap/step versions
+	set_versions(table, snapshot_version+1, 0);
+		
+	db_key_t keys[1];
+	db_val_t vals[1];
+	db_op_t ops[1];
+	db_key_t result_cols[1];
+	
+	db_res_t *res = NULL;
+	
+	keys[0] = "snapshot_version";
+	
+	ops[0] = OP_EQ;
+
+	vals[0].type = DB_INT;
+	vals[0].nul = 0;
+	vals[0].val.int_val = snapshot_version;
+	
+	result_cols[0]="data";
+	
+	if (scscf_dbf.use_table(scscf_db, table) < 0) {
+		LOG(L_ERR, "ERR:"M_NAME":bin_load_from_table(): Error in use_table\n");
+		lock_release(db_lock);//unlock
+		return -1;
+	}
+	
+	if (scscf_dbf.query(scscf_db, keys, ops, vals, result_cols, 1, 1, 0, &res) < 0) {
+		LOG(L_ERR, "ERR:"M_NAME":bin_load_from_table(): Error while querying %s\n", table);
+		lock_release(db_lock);//unlock
+		return -1;
+	}
+	
+	if (!res) {
+		lock_release(db_lock);//unlock
+		return 0;
+	}
+	
+	if(res->n==0){
+		scscf_dbf.free_result(scscf_db, res);
+		lock_release(db_lock);//unlock
+		return 1;
+	}
+	
+	db_row_t* rows = RES_ROWS(res);
+	db_val_t *row_vals = ROW_VALUES(rows);
+	
+	int len=row_vals[0].val.blob_val.len;
+	LOG(L_INFO,"INFO:"M_NAME":%s row length -> %d\n", table, len);
+	bin_expand(x, len);
+	
+	memcpy(x->s, row_vals[0].val.blob_val.s, len);
+	x->len+=len;
+	
+	lock_release(db_lock);//unlock
+	
+	return 1;
+}
+
+
+/**
+ * Gets the version of the last snapshot dumped to db.
+ * Used to load information form db on startup.
+ * @param table - the table to query
+ * @param version - where to load
+ * @returns 1 on success, -1 if empty table, 0 on error
+ */
+int db_get_last_snapshot_version(char* table, int* version){
+	
+	db_res_t *res = NULL;
+	char sql[50];
+	
+	sprintf(sql, "SELECT max(snapshot_version) from %s", table);
+	
+	if (scscf_dbf.raw_query(scscf_db, sql, &res) < 0) {
+		LOG(L_ERR, "ERR:"M_NAME":db_get_last_snapshot_version(): Error while getting last snapshot version from %s\n", table);
+		return 0;
+	}
+	
+	if (!res) return 0;
+	
+	/*if(res->n==0) { //never happens with MAX function
+		LOG(L_ERR, "ERR:"M_NAME":db_get_last_snapshot_version(): %s empty\n", table);
+		scscf_dbf.free_result(scscf_db, res);
+		return -1;
+	}*/
+	
+	db_row_t* rows = RES_ROWS(res);
+	db_val_t *row_vals = ROW_VALUES(rows);
+	
+	//MAX/MIN of an empty table will return a row of value NULL
+	if(VAL_NULL(&row_vals[0])) {
+		LOG(L_INFO, "INFO:"M_NAME":db_get_last_snapshot_version(): %s empty\n", table);
+		scscf_dbf.free_result(scscf_db, res);
+		return -1;
+	}
+	
+	*version=row_vals[0].val.int_val;
+	
+	LOG(L_INFO, "INFO:"M_NAME":db_get_last_snapshot_version(): %s -> %d\n", table, *version);
+	
+	scscf_dbf.free_result(scscf_db, res);
+	
+	return 1;
+}
+
+/**
+ * @param table - auth, dialogs or registrar versions?
+ * @param snapshot_version - it must continue from the last snapshot version + 1
+ * @param step_version - it must continue from the last step version + 1 
+ * @returns 1 on success, 0 on error
+ */
+int set_versions(char* table, int snapshot_version, int step_version){
+
+	if(!strncmp(table,"registrar_bulk",14)){
+		*registrar_snapshot_version=snapshot_version;
+		*registrar_step_version=step_version;
+	}
+	else{
+		if(!strncmp(table,"dialogs_bulk",12)){
+			*dialogs_snapshot_version=snapshot_version;
+			*dialogs_step_version=step_version;
+		}
+		else{
+			if(!strncmp(table,"auth_data_bulk",14)){
+				*auth_snapshot_version=snapshot_version;
+				*auth_step_version=step_version;
+			}
+			else{
+				LOG(L_ERR,"ERR:"M_NAME"set_versions: No such information %s\n", table);
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
 
 /**
  * Timer callback for persistency dumps
@@ -360,7 +565,8 @@ int load_snapshot_dialogs()
 {
 	bin_data x;
 	s_dialog *d;
-	if (!bin_load(&x,scscf_persistency_mode,scscf_persistency_location,"sdialogs")) goto error;
+	if (!s_load(&x,scscf_persistency_mode,scscf_persistency_location,"sdialogs")) goto error;
+	//if (!bin_load(&x,scscf_persistency_mode,scscf_persistency_location,"sdialogs")) goto error;
 	bin_print(&x);
 	x.max=0;
 	LOG(L_INFO,"INFO:"M_NAME":load_snapshot_dlg: max %d len %d\n",x.max,x.len);
@@ -437,7 +643,8 @@ int load_snapshot_registrar()
 {
 	bin_data x;
 	r_public *p;
-	if (!bin_load(&x,scscf_persistency_mode,scscf_persistency_location,"sregistrar")) goto error;
+	if (!s_load(&x,scscf_persistency_mode,scscf_persistency_location,"sregistrar")) goto error;
+	//if (!bin_load(&x,scscf_persistency_mode,scscf_persistency_location,"sregistrar")) goto error;
 	bin_print(&x);
 	x.max=0;
 	LOG(L_INFO,"INFO:"M_NAME":load_snapshot_registrar: max %d len %d\n",x.max,x.len);
