1948a68dacf4c05286c1e70e830e717d3b1e4b9c
mariojvp <mariojvp@a36f3ba7-d720-0410-92d7-d12b36172079>
2007-02-27 12:17:59 +0000

    Added WITH_DATABASE_CACHE mode (to allow the cache implementation). Each hashtable element is dumped to DB separately. Dump of auth, registrar and dialogs information to DB. This information is loaded at S-CSCF startup.

--- ser_ims/branches/01_persistency/cfg/pg_scscf.sql
+++ ser_ims/branches/01_persistency/cfg/pg_scscf.sql
@@ -10,7 +10,7 @@ CREATE TABLE auth_data_cache (
     step_version INTEGER NOT NULL,
     private VARCHAR(80) NOT NULL,
     public  VARCHAR(128) NOT NULL,
-    data BYTEA,
+    data BYTEA NOT NULL,
     PRIMARY KEY (snapshot_version, step_version, private, public)
 );
 
@@ -21,9 +21,26 @@ CREATE TABLE dialogs_bulk (
     PRIMARY KEY (snapshot_version, step_version)
 );
 
+CREATE TABLE dialogs_cache (
+    snapshot_version INTEGER NOT NULL,
+    step_version INTEGER NOT NULL,
+    call_id VARCHAR(256) NOT NULL,
+    aor VARCHAR(128) NOT NULL,
+    data BYTEA NOT NULL,
+    PRIMARY KEY (snapshot_version, step_version, call_id, aor)
+);
+
 CREATE TABLE registrar_bulk (
     snapshot_version INTEGER NOT NULL,
     step_version INTEGER NOT NULL,
     data BYTEA,
     PRIMARY KEY (snapshot_version, step_version)
+);
+
+CREATE TABLE registrar_cache (
+    snapshot_version INTEGER NOT NULL,
+    step_version INTEGER NOT NULL,
+    aor VARCHAR(128) NOT NULL,
+    data BYTEA NOT NULL,
+    PRIMARY KEY (snapshot_version, step_version, aor)
 );
