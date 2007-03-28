328e2915a0f73aca40e94e29aa215f22b4af668b
vingarzan <vingarzan@a36f3ba7-d720-0410-92d7-d12b36172079>
2007-03-28 13:49:58 +0000

    Created branch for ratelimit work.

---
+++ ser_ims/branches/03_ratelimit/cfg/my_scscf.sql
@@ -0,0 +1,46 @@
+CREATE TABLE auth_data_bulk (
+    snapshot_version INT NOT NULL,
+    step_version INT NOT NULL,
+    data BLOB,
+    PRIMARY KEY (snapshot_version, step_version)
+);
+
+CREATE TABLE auth_data_cache (
+    snapshot_version INT NOT NULL,
+    step_version INT NOT NULL,
+    private VARCHAR(80) NOT NULL,
+    public  VARCHAR(128) NOT NULL,
+    data BLOB,
+    PRIMARY KEY (snapshot_version, step_version, private, public)
+);
+
+CREATE TABLE dialogs_bulk (
+    snapshot_version INT NOT NULL,
+    step_version INT NOT NULL,
+    data BLOB,
+    PRIMARY KEY (snapshot_version, step_version)
+);
+
+CREATE TABLE dialogs_cache (
+    snapshot_version INT NOT NULL,
+    step_version INT NOT NULL,
+    call_id VARCHAR(256) NOT NULL,
+    aor VARCHAR(128) NOT NULL,
+    data BLOB NOT NULL,
+    PRIMARY KEY (snapshot_version, step_version, call_id, aor)
+);
+
+CREATE TABLE registrar_bulk (
+    snapshot_version INT NOT NULL,
+    step_version INT NOT NULL,
+    data BLOB,
+    PRIMARY KEY (snapshot_version, step_version)
+);
+
+CREATE TABLE registrar_cache (
+    snapshot_version INT NOT NULL,
+    step_version INT NOT NULL,
+    aor VARCHAR(128) NOT NULL,
+    data BLOB NOT NULL,
+    PRIMARY KEY (snapshot_version, step_version, aor)
+);
