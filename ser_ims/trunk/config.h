d54b9d8d6d82a7eed4f9535d6ed05d9673521f76
vingarzan <vingarzan@a36f3ba7-d720-0410-92d7-d12b36172079>
2007-07-26 14:39:50 +0000

    Increased the memory size per process from 4 to 8 MBytes.

--- ser_ims/trunk/config.h
+++ ser_ims/trunk/config.h
@@ -129,7 +129,7 @@
 #define SRV_MAX_PREFIX_LEN SRV_TLS_PREFIX_LEN
 
 /*used only if PKG_MALLOC is defined*/
-#define PKG_MEM_POOL_SIZE 4*1024*1024
+#define PKG_MEM_POOL_SIZE 8*1024*1024
 
 /*used if SH_MEM is defined*/
 #define SHM_MEM_SIZE 32
