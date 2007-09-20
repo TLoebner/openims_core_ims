8b54df867382ba09606a79cbce511c872648345c
emoreno <emoreno@a36f3ba7-d720-0410-92d7-d12b36172079>
2007-09-20 09:46:57 +0000

    Small correction at line #110 in de.fhg.fokus.hss.web.action.SP_Load.java class, and deletion of unnecessary line #130 (see r447).

--- FHoSS/branches/07_Dynamic_Service_Activation/src/de/fhg/fokus/hss/web/action/SP_Load.java
+++ FHoSS/branches/07_Dynamic_Service_Activation/src/de/fhg/fokus/hss/web/action/SP_Load.java
@@ -33,12 +33,12 @@
   * fact and have to agree to check out carefully before installing,
   * using and extending the Open Source IMS Core System, if related
   * patents and licenses may become applicable to the intended usage
-  * context. 
+  * context.
   *
   * You should have received a copy of the GNU General Public License
   * along with this program; if not, write to the Free Software
-  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA  
-  * 
+  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  *
   */
 
 package de.fhg.fokus.hss.web.action;
@@ -68,7 +68,7 @@ import de.fhg.fokus.hss.web.form.SP_Form;
 import de.fhg.fokus.hss.web.util.WebConstants;
 
 /**
- * @author adp dot fokus dot fraunhofer dot de 
+ * @author adp dot fokus dot fraunhofer dot de
  * Adrian Popescu / FOKUS Fraunhofer Institute
  */
 
@@ -78,57 +78,56 @@ public class SP_Load extends Action {
 
 	public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm,
 			HttpServletRequest request, HttpServletResponse reponse) {
-		
+
 		SP_Form form = (SP_Form) actionForm;
 		int id = form.getId();
 		List attached_ifc_list;
 		List attached_shared_ifc_list;
-		
+
 		List select_ifc = new ArrayList();
 		List select_shared_ifc = new ArrayList();
 		ActionForward forward = null;
-		
+
 		boolean dbException = false;
 		try{
 			Session session = HibernateUtil.getCurrentSession();
 			HibernateUtil.beginTransaction();
-			
+
 			// load
-			SP sp = SP_DAO.get_by_ID(session, id); 
+			SP sp = SP_DAO.get_by_ID(session, id);
 			SP_Load.setForm(form, sp);
-			
+
 			// set select_ifc & select_shared_ifc
 			select_ifc = IFC_DAO.get_all(session);
 			form.setSelect_ifc(select_ifc);
 
-			select_shared_ifc = Shared_IFC_Set_DAO.get_all_Sets(session);	
+			select_shared_ifc = Shared_IFC_Set_DAO.get_all_Sets(session);
 			form.setSelect_shared_ifc(select_shared_ifc);
-			
+
 			attached_ifc_list = SP_IFC_DAO.get_all_IFC_by_SP_ID(session, id);
 			attached_shared_ifc_list = SP_Shared_IFC_Set_DAO.get_all_Shared_IFC_by_SP_ID(session, id);
-			
-			if (attached_shared_ifc_list != null){
+
+			if (attached_ifc_list != null){
 				request.setAttribute("attached_ifc_list", attached_ifc_list);
 			}
 			else{
 				request.setAttribute("attached_ifc_list", new ArrayList());
 			}
-			
+
 			if (attached_shared_ifc_list != null){
 				request.setAttribute("attached_shared_ifc_list", attached_shared_ifc_list);
 			}
 			else{
 				request.setAttribute("attached_shared_ifc_list", new ArrayList());
 			}
-			
+
 			if (SP_Load.testForDelete(session, form.getId())){
 				request.setAttribute("deleteDeactivation", "false");
 			}
 			else{
 				request.setAttribute("deleteDeactivation", "true");
 			}
-			request.setAttribute("attached_IFC_list", attached_ifc_list);
-			
+
 			forward = actionMapping.findForward(WebConstants.FORWARD_SUCCESS);
 			forward = new ActionForward(forward.getPath() + "?id=" + id);
 		}
@@ -136,14 +135,14 @@ public class SP_Load extends Action {
 			logger.error("Database Exception occured!\nReason:" + e.getMessage());
 			e.printStackTrace();
 			dbException = true;
-			
+
 			forward = actionMapping.findForward(WebConstants.FORWARD_FAILURE);
 		}
 		catch (HibernateException e){
 			logger.error("Hibernate Exception occured!\nReason:" + e.getMessage());
 			e.printStackTrace();
 			dbException = true;
-			
+
 			forward = actionMapping.findForward(WebConstants.FORWARD_FAILURE);
 		}
 		finally{
@@ -151,12 +150,12 @@ public class SP_Load extends Action {
 				HibernateUtil.commitTransaction();
 			}
 			HibernateUtil.closeSession();
-		}		
-		
+		}
+
 		return forward;
 	}
-	
-	
+
+
 	public static boolean testForDelete(Session session, int id){
 		List result = SP_IFC_DAO.get_all_IFC_by_SP_ID(session, id);
 		if (result != null && result.size() > 0){
@@ -166,20 +165,20 @@ public class SP_Load extends Action {
 		if (result != null && result.size() > 0){
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	public static boolean setForm(SP_Form form, SP sp){
 		boolean exitCode = false;
-		
+
 		if (sp != null){
 			exitCode = true;
 			form.setId(sp.getId());
 			form.setName(sp.getName());
 			form.setCn_service_auth(sp.getCn_service_auth());
-			
+
 		}
 		return exitCode;
-	}	
+	}
 }
