29a4d52f71e01bdc034138323873dfb550d3a7c2
aon <aon@a36f3ba7-d720-0410-92d7-d12b36172079>
2011-02-16 10:29:53 +0000

    added Client_Rf module for the Rf interface from the P/I/S/E-CSCF and other components towards the charging components

---
+++ ser_ims/trunk/modules/Client_Rf/mod.c
@@ -0,0 +1,268 @@
+/*
+ * Copyright (C) 2008-2009 FhG Fokus
+ *
+ * This file is part of Open IMS Core - an open source IMS CSCFs & HSS
+ * implementation
+ *
+ * Open IMS Core is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * For a license to use the Open IMS Core software under conditions
+ * other than those described here, or to purchase support for this
+ * software, please contact Fraunhofer FOKUS by e-mail at the following
+ * addresses:
+ *     info@open-ims.org
+ *
+ * Open IMS Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * It has to be noted that this Open Source IMS Core System is not 
+ * intended to become or act as a product in a commercial context! Its 
+ * sole purpose is to provide an IMS core reference implementation for 
+ * IMS technology testing and IMS application prototyping for research 
+ * purposes, typically performed in IMS test-beds.
+ * 
+ * Users of the Open Source IMS Core System have to be aware that IMS
+ * technology may be subject of patents and licence terms, as being 
+ * specified within the various IMS-related IETF, ITU-T, ETSI, and 3GPP
+ * standards. Thus all Open IMS Core users have to take notice of this 
+ * fact and have to agree to check out carefully before installing, 
+ * using and extending the Open Source IMS Core System, if related 
+ * patents and licences may become applicable to the intended usage 
+ * context.  
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ * author Ancuta Onofrei, 
+ * 	email andreea dot ancuta dot onofrei -at- fokus dot fraunhofer dot de
+ */
+/**
+ * Client_Rf - implementation of the Rf interface from the CTF side, according to TS32.299 R7
+ * 
+ * Scope:
+ * - Exports parameters and functions
+ * - Initialization functions
+ * 
+ */
+ 
+
+#include <ctype.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "mod.h"
+
+#include "../../parser/parse_uri.h"
+#include "../../sr_module.h"
+#include "../../socket_info.h"
+#include "../../timer.h"
+#include "../../locking.h"
+#include "../../modules/tm/tm_load.h"
+#ifdef SER_MOD_INTERFACE
+	#include "../../modules_s/dialog/dlg_mod.h"
+#else
+	#include "../dialog/dlg_mod.h"
+#endif
+
+#include "../cdp/cdp_load.h"
+#include "../cdp_avp/mod_export.h"
+
+#include "diameter_rf.h"
+
+MODULE_VERSION
+
+static int mod_init(void);
+static int mod_child_init(int rank);
+static void mod_destroy(void);
+
+/* parameters storage */
+int * shutdown_singleton;
+
+
+/** 
+ * Exported functions.
+ *
+ */
+static cmd_export_t client_rf_cmds[]={
+//	{"Send_ACR",			Send_ACR, 			1, 0, REQUEST_ROUTE|ONREPLY_ROUTE|FAILURE_ROUTE},
+	{0, 0, 0, 0, 0}
+}; 
+
+/** 
+ * Exported parameters.
+ */	
+static param_export_t client_rf_params[]={ 
+	{0,0,0} 
+};
+
+/** module exports */
+struct module_exports exports = {
+	"Client_Rf", 
+	client_rf_cmds,
+	0,
+	client_rf_params,
+	mod_init,			/* module initialization function */
+	0,				/* response function*/
+	mod_destroy,			/* destroy function */
+	0,				/* onbreak function */
+	mod_child_init			/* per-child init function */
+};
+
+
+/* Global variables and imported functions */
+struct cdp_binds cdpb;					/**< Structure with pointers to cdp funcs				*/
+cdp_avp_bind_t *cdp_avp=0;
+
+/**< link to the stateless reply function in sl module */
+int (*sl_reply)(struct sip_msg* _msg, char* _str1, char* _str2); 
+
+/**< Structure with pointers to tm funcs 		*/
+struct tm_binds tmb;            						
+dlg_func_t dialogb;	
+
+/**
+ * Fix the configuration parameters.
+ */
+int fix_parameters()
+{
+	LOG(L_INFO,"INFO:"M_NAME":mod_init: Initialization of module\n");
+
+	return 1;
+}
+
+
+/**
+ * Initializes the module.
+ */
+static int mod_init(void)
+{
+	load_tm_f load_tm;
+	load_cdp_f load_cdp;
+	cdp_avp_get_bind_f load_cdp_avp;
+	bind_dlg_mod_f load_dlg;
+			
+	LOG(L_INFO,"INFO:"M_NAME":mod_init: Initialization of module\n");
+	shutdown_singleton=shm_malloc(sizeof(int));
+	*shutdown_singleton=0;
+	
+	
+	/* fix the parameters */
+	if (!fix_parameters()) goto error;
+
+	/* load the send_reply function from sl module */
+    	sl_reply = find_export("sl_send_reply", 2, 0);
+	if (!sl_reply) {
+		LOG(L_ERR, "ERR"M_NAME":mod_init: This module requires sl module\n");
+		goto error;
+	}
+	
+	/* bind to the tm module */
+	if (!(load_tm = (load_tm_f)find_export("load_tm",NO_SCRIPT,0))) {
+		LOG(L_ERR, "ERR:"M_NAME":mod_init: Can not import load_tm. This module requires tm module\n");
+		goto error;
+	}
+	if (load_tm(&tmb) == -1)
+		goto error;
+
+	/* bind to the dialog module */
+	load_dlg = (bind_dlg_mod_f)find_export("bind_dlg_mod", -1, 0);
+	if (!load_dlg) {
+		LOG(L_ERR, "ERR"M_NAME":mod_init:  Can not import bind_dlg_mod. This module requires dialog module\n");
+		return -1;
+	}
+	if (load_dlg(&dialogb) != 0) {
+		return -1;
+	}
+
+	if (!(load_cdp = (load_cdp_f)find_export("load_cdp",NO_SCRIPT,0))) {
+		LOG(L_ERR, "DBG:"M_NAME":mod_init: Can not import load_cdp. This module requires cdp module.\n");
+		goto error;
+	}
+
+	if (load_cdp(&cdpb) == -1){
+		LOG(L_ERR, "DBG:"M_NAME":mod_init: loading cdp module unsuccessful. exiting\n");
+		goto error;
+	}
+		
+	if (!(load_cdp_avp = (cdp_avp_get_bind_f)find_export("cdp_avp_get_bind",NO_SCRIPT,0))) {
+		LOG(L_ERR, "DBG:"M_NAME":mod_init: loading cdp_avp module unsuccessful. exiting\n");			
+		goto error;
+	}	
+	cdp_avp = load_cdp_avp();
+	if (!cdp_avp)
+		goto error;
+
+
+	return 0;
+error:
+	return -1;
+}
+
+extern gen_lock_t* process_lock;		/* lock on the process table */
+
+/**
+ * Initializes the module in child.
+ */
+static int mod_child_init(int rank)
+{
+	LOG(L_INFO,"INFO:"M_NAME":mod_init: Initialization of module in child [%d] \n",
+		rank);
+	/* don't do anything for main process and TCP manager process */
+	if ( rank == PROC_MAIN || rank == PROC_TCP_MAIN )
+		return 0;
+
+	lock_get(process_lock);
+		cdpb.AAAAddResponseHandler(RfChargingResponseHandler, NULL);
+	lock_release(process_lock);
+			
+	return 0;
+}
+
+extern gen_lock_t* process_lock;		/* lock on the process table */
+
+/**
+ * Destroys the module.
+ */
+static void mod_destroy(void)
+{
+	int do_destroy=0;
+	LOG(L_INFO,"INFO:"M_NAME":mod_destroy: child exit\n");
+	
+	lock_get(process_lock);
+	if((*shutdown_singleton)==0){
+		*shutdown_singleton=1;
+		do_destroy=1;
+	}
+	lock_release(process_lock);
+
+	if (do_destroy){
+		/* Then nuke it all */	
+	}
+	
+}
+
+
+/**
+ * Checks if the transaction is in processing.
+ * @param msg - the SIP message to check
+ * @param str1 - not used
+ * @param str2 - not used
+ * @returns #CSCF_RETURN_TRUE if the transaction is already in processing, #CSCF_RETURN_FALSE if not
+ */
+int trans_in_processing(struct sip_msg* msg, char* str1, char* str2)
+{
+	unsigned int hash, label;
+	if (tmb.t_get_trans_ident(msg,&hash,&label)<0)
+		return CSCF_RETURN_FALSE;
+	return CSCF_RETURN_TRUE;	
+}
+
