003b1380889d7e8d61517c5872f078d881b3988c
aon <aon@a36f3ba7-d720-0410-92d7-d12b36172079>
2011-02-16 10:29:53 +0000

    added Client_Rf module for the Rf interface from the P/I/S/E-CSCF and other components towards the charging components

---
+++ ser_ims/trunk/modules/Client_Rf/diameter_rf.h
@@ -0,0 +1,10 @@
+
+#ifndef __CLIENT_RF_DIAMETER_RF_H
+#define __CLIENT_RF_DIAMETER_RF_H
+
+#include "../cdp/cdp_load.h"
+#include "../cdp_avp/mod_export.h"
+
+void RfChargingResponseHandler(AAAMessage *response,void *param);
+
+#endif /* __CLIENT_RF_DIAMETER_RF_H */
